e9da1525655406fa3e5a00841b1bc045770e7303
Misphe <124389712+Misphe@users.noreply.github.com>
2024-10-28 14:45:31 +0100

    reports changes

--- generator.py
+++ generator.py
@@ -9,38 +9,38 @@ from classes.van_route_generator import VanRoutesGenerator
 from classes.vehicle_generator import VehiclesGenerator
 
 def generate_drivers():
-    DriverGenerator(10).generate_and_save('generated_data/drivers2.csv')
+    DriverGenerator(100).generate_and_save('generated_data/drivers.csv')
 
 def generate_stations(start_index):
-    StationsGenerator(10, start_index).generate_and_save('generated_data/stations_data2.csv')
+    StationsGenerator(100, start_index).generate_and_save('generated_data/stations_data.csv')
 
 def generate_users(start_index):
-    UserGenerator(10, start_index).generate_and_save('generated_data/users2.csv')
+    UserGenerator(100, start_index).generate_and_save('generated_data/users.csv')
 
 def generate_vans():
-    VansGenerator(10).generate_and_save('generated_data/vans_data2.csv')
+    VansGenerator(100).generate_and_save('generated_data/vans_data.csv')
 
 def generate_vehicles(start_index):
-    VehiclesGenerator(10, start_index=start_index).generate_and_save('generated_data/vehicles_data2.csv')
+    VehiclesGenerator(100, start_index=start_index).generate_and_save('generated_data/vehicles_data.csv')
 
 def generate_rentals(start_index):
-    RentalGenerator(stations_filename='generated_data/stations_data12.csv',
-                    vehicles_filename='generated_data/vehicles_data12.csv',
-                    users_filename='generated_data/users12.csv',
-                    num_rentals=100,
-                    start_index=start_index).generate_and_save('generated_data/rentals_data2.csv')
+    RentalGenerator(stations_filename='generated_data/stations_data.csv',
+                    vehicles_filename='generated_data/vehicles_data.csv',
+                    users_filename='generated_data/users.csv',
+                    num_rentals=1000,
+                    start_index=start_index).generate_and_save('generated_data/rentals_data.csv')
 
 def generate_van_routes(start_index):
-    VanRoutesGenerator(drivers_filename='generated_data/drivers12.csv',
-                       vans_filename='generated_data/vans_data12.csv',
-                       num_routes=100,
-                       start_index=start_index).generate_and_save('generated_data/van_routes_data2.csv')
+    VanRoutesGenerator(drivers_filename='generated_data/drivers.csv',
+                       vans_filename='generated_data/vans_data.csv',
+                       num_routes=1000,
+                       start_index=start_index).generate_and_save('generated_data/van_routes_data.csv')
 
 def generate_battery_replacements(start_index):
-    BatteryReplacementGenerator(van_routes_csv='generated_data/van_routes_data12.csv',
-                                vehicles_csv='generated_data/vehicles_data12.csv',
-                                num_replacements=100,
-                                start_index=start_index).generate_and_save('generated_data/battery_replacements_data2.csv')
+    BatteryReplacementGenerator(van_routes_csv='generated_data/van_routes_data.csv',
+                                vehicles_csv='generated_data/vehicles_data.csv',
+                                num_replacements=1000,
+                                start_index=start_index).generate_and_save('generated_data/battery_replacements_data.csv')
 
 mode = "facts"
 mode2 = "last"
@@ -50,10 +50,10 @@ if __name__ == "__main__":
     if mode != "facts":
         first_wave_threads = [
             threading.Thread(target=generate_drivers),
-            threading.Thread(target=generate_stations, args=(101,)),
-            threading.Thread(target=generate_users, args=(101,)),
+            threading.Thread(target=generate_stations, args=(1,)),
+            threading.Thread(target=generate_users, args=(1,)),
             threading.Thread(target=generate_vans),
-            threading.Thread(target=generate_vehicles, args=(101,))
+            threading.Thread(target=generate_vehicles, args=(1,))
         ]
 
         for thread in first_wave_threads:
@@ -65,8 +65,8 @@ if __name__ == "__main__":
     if mode2 != "last":
         # Second wave
         second_wave_threads = [
-            threading.Thread(target=generate_rentals, args=(1001,)),
-            threading.Thread(target=generate_van_routes, args=(1001,))
+            threading.Thread(target=generate_rentals, args=(1,)),
+            threading.Thread(target=generate_van_routes, args=(1,))
         ]
 
         for thread in second_wave_threads:
@@ -77,7 +77,7 @@ if __name__ == "__main__":
 
     # Third wave
     third_wave_threads = [
-        threading.Thread(target=generate_battery_replacements, args=(1001,))
+        threading.Thread(target=generate_battery_replacements, args=(1,))
     ]
 
     for thread in third_wave_threads:
